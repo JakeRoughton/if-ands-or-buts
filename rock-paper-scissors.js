09be688d312c76636076f9a84f0743b535fcb9d4
JakeRoughton <112968037+JakeRoughton@users.noreply.github.com>
2022-10-02 23:43:01 -0400

    Add files via upload

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,18 +3,18 @@ let player1 = prompt("What is your name Player 1: ");
 let player2 = prompt("And Player 2, who are you: ");
 console.log();
 
-player1 = prompt("Choose your weapon: rock, paper, or scissors: ")
+player1 = prompt(`Choose your weapon ${player1}: rock, paper, or scissors: `)
 
 if(player1 === "rock"){
     console.log("You picked rock. SMAAASH SCISSORS!!!!");
 } else if(player1 === "paper"){
-    console.log("You picked paper. COVER THAT ROCK!!!")
+    console.log("You picked paper. COVER THAT ROCK!!!");
 } else if(player1 === "scissors"){
         console.log("You picked scissors. SLICE AND DICE PAPER!")
 }
 
 
-player2 = prompt ("And how will you defend yourself: ")
+player2 = prompt (`And how will you defend yourself ${player2}: `);
 
 if(player2 === "rock"){
     console.log("You picked rock. SMAAASH SCISSORS!!!!");
@@ -26,11 +26,16 @@ if(player2 === "rock"){
 
 if(player1 === "rock" && player2 === "rock"){
     console.log("draw")
+}   else if(player1 === "scissors" && player2 === "scissors"){
+    console.log("draw")
+}   else if(player1 === "paper" && player2 === "paper"){
+    console.log("draw")
 }   else if(player1 === "rock" && player2 === "paper"){
     console.log("Player 2 WINS")
 }   else if(player1 === "rock" && player2 === "scissors"){
-    console.log("Player 1 LOSES")
+    console.log("Player 1 WINS")
 }   else if(player2 === "rock" && player1 === "paper"){
-   console.log("Player 2 LOSES")
-}   else(player1 === "scissors" && player2 === "rock")
-    console.log("Player 2 WINS")
+    console.log("Player 1 WINS")
+}   else if(player1 === "scissors" && player2 === "rock"){
+    console.log("Player 2 WINS");
+}
